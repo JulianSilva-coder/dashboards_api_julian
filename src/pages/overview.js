73953ba261ec59629dbf2fe57cbd0e53e5a4fed6
JulianSilva-coder <jsilvabe@unbosque.edu.co>
2025-06-10 23:14:17 -0500

    version final

--- src/pages/overview.js
+++ src/pages/overview.js
@@ -2,20 +2,21 @@ import ProjectList from './components/ProjectList';
 import SourceList from './components/SourceList';
 import PanelList from './components/PanelList';
 import DashboardList from './components/DashboardList';
+import '../index.css';
 
 export default function Overview() {
   return (
     <div className="container">
-      <div className="section">
+      <div className="section project-section">
         <ProjectList />
       </div>
-      <div className="section">
+      <div className="section source-section">
         <SourceList />
       </div>
-      <div className="section">
+      <div className="section panel-section">
         <PanelList />
       </div>
-      <div className="section">
+      <div className="section dashboard-section">
         <DashboardList />
       </div>
     </div>
